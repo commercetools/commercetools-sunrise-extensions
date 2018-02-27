d48e924acd96a171a3432999649219bcd7109e20
Laura Luiz <laura.luiz@commercetools.de>
2018-02-27 11:49:03 +0100

    Print simple hello world in controller

--- src/main/java/com/commercetools/sunrise/extensions/HelloWorldController.java
+++ src/main/java/com/commercetools/sunrise/extensions/HelloWorldController.java
@@ -1,16 +1,16 @@
 package com.commercetools.sunrise.extensions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
 
-    @GetMapping(value = "/hello", produces = "text/html")
-    public ResponseEntity<String> greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
-        return new ResponseEntity<>("hi", HttpStatus.OK);
+    @GetMapping(value = "/hello")
+    @ResponseBody
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        return "Hello World!";
     }
 }
