6ef242ff3bad57689c0d8b2bcfbf9a6592cbd2fd
Laura Luiz <laura.luiz@commercetools.de>
2018-02-27 13:00:13 +0100

    Accept JSON request input

---
+++ src/main/java/com/commercetools/sunrise/extensions/ExtensionRequest.java
@@ -0,0 +1,15 @@
+package com.commercetools.sunrise.extensions;
+
+public class ExtensionRequest {
+
+    private String action;
+    //private final Reference<>
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(final String action) {
+        this.action = action;
+    }
+}
--- src/main/java/com/commercetools/sunrise/extensions/HelloWorldController.java
+++ src/main/java/com/commercetools/sunrise/extensions/HelloWorldController.java
@@ -2,9 +2,10 @@ package com.commercetools.sunrise.extensions;
 
 import io.sphere.sdk.client.BlockingSphereClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,9 +14,9 @@ public class HelloWorldController {
     @Autowired
     private BlockingSphereClient sphereClient;
 
-    @GetMapping(value = "/hello")
+    @PostMapping(value = "/hello", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
-        return "Hello World!";
+    public String greeting(@RequestBody ExtensionRequest request) {
+        return "Hello " + request.getAction();
     }
 }
