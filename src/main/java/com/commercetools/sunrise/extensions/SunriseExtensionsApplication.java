da0f496af08d59ddb4be7cb900fbeababff31b8a
schleichardt <michael.schleichardt@commercetools.de>
2018-02-27 10:55:56 +0100

    init spring boot app

---
+++ src/main/java/com/commercetools/sunrise/extensions/SunriseExtensionsApplication.java
@@ -0,0 +1,12 @@
+package com.commercetools.sunrise.extensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseExtensionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunriseExtensionsApplication.class, args);
+	}
+}
