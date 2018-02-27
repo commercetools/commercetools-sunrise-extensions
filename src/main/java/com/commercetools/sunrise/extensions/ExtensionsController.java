0d43bd44e5b05d6385e2083cfd30c770e909e9fa
Laura Luiz <laura.luiz@commercetools.de>
2018-02-27 16:59:42 +0100

    Add some logging

--- src/main/java/com/commercetools/sunrise/extensions/ExtensionsController.java
+++ src/main/java/com/commercetools/sunrise/extensions/ExtensionsController.java
@@ -4,6 +4,7 @@ import io.sphere.sdk.carts.Cart;
 import io.sphere.sdk.carts.commands.updateactions.SetCustomerEmail;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.customers.queries.CustomerByIdGet;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,7 @@ import java.util.concurrent.CompletionStage;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 
 @Controller
+@Slf4j
 public class ExtensionsController {
 
     @Autowired
@@ -24,13 +26,22 @@ public class ExtensionsController {
     @PostMapping(value = "/carts", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public CompletionStage<ExtensionResponse<Cart>> carts(@RequestBody ExtensionRequest<Cart> request) {
+        log.info("Starting cart extension");
         final Cart cart = extractCart(request);
         final boolean needsCustomerEmail = cart.getCustomerEmail() == null && cart.getCustomerId() != null;
         if (needsCustomerEmail) {
-            return sphereClient.execute(CustomerByIdGet.of(cart.getCustomerId()))
-                    .thenApply(customer -> new UpdatesExtensionResponse<>(SetCustomerEmail.of(customer.getEmail())));
+            log.info("Setting customer email to cart...");
+            return setCustomerEmail(cart.getCustomerId());
         }
+        log.info("Nothing to do");
         return completedFuture(new NoOpExtensionResponse<>());
+    }
+
+    private CompletionStage<ExtensionResponse<Cart>> setCustomerEmail(final String customerId) {
+        final CompletionStage<ExtensionResponse<Cart>> response = sphereClient.execute(CustomerByIdGet.of(customerId))
+                .thenApply(customer -> new UpdatesExtensionResponse<>(SetCustomerEmail.of(customer.getEmail())));
+        response.thenAccept(customer -> log.info("Customer email set successfully"));
+        return response;
     }
 
     private Cart extractCart(final ExtensionRequest<Cart> request) {
