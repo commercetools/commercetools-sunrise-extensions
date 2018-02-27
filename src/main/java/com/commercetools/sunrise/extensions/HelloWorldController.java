a23372654afc16e7b6ff8d4475875ccbe1260571
Laura Luiz <laura.luiz@commercetools.de>
2018-02-27 11:27:29 +0100

    Fix hello world controller

--- src/main/java/com/commercetools/sunrise/extensions/HelloWorldController.java
+++ src/main/java/com/commercetools/sunrise/extensions/HelloWorldController.java
@@ -1,16 +1,16 @@
 package com.commercetools.sunrise.extensions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping(value = "/hello", produces = "text/html")
+    public ResponseEntity<String> greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        return new ResponseEntity<>("hi", HttpStatus.OK);
     }
 }
