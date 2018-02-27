2ab42aa3a459dcbc8ccb40631e29b5aac0dead1a
schleichardt <michael.schleichardt@commercetools.de>
2018-02-27 11:56:53 +0100

    in production get ctp credentials from environment variables

--- src/main/java/com/commercetools/sunrise/extensions/HelloWorldController.java
+++ src/main/java/com/commercetools/sunrise/extensions/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.commercetools.sunrise.extensions;
 
+import io.sphere.sdk.client.BlockingSphereClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
+    @Autowired
+    BlockingSphereClient sphereClient;
 
     @RequestMapping("/")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
